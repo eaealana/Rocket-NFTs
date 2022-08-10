2f95b2d2f0076deaf896eeac428884f2ac7df3b8
Alana <ribeiroalana2005@gmail.com>
2022-08-10 16:48:06 -0300

    tidying up semantics

--- js.js
+++ js.js
@@ -1,26 +1,9 @@
 const swiper = new Swiper('.mySwiper', {
     slidesPerView: 2,
+    loop: true,
     scrollbar: {
         el: '.swiper-scrollbar',
-        hide: true
     },
     mousewheel: true,
-    heyboard: true,
-    breakpoints: {
-        776: {
-            slidesPerView: 2,
-            setWrapperSize: true
-        },
-
-        992: {
-            slidesPerView: 2,
-            setWrapperSize: true,
-        },
-
-        1200: {
-            slidesPerView: 2,
-            setWrapperSize: true
-        }
-    }
 })
 
